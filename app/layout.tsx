c293b945b8259d0c5ab5608cf88aa0e815c82e9a
Igor Telles Barbosa <igor.telles@ibm.com>
2024-05-20 11:25:38 -0300

    no srr no errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,19 @@
 "use client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { UrqlProvider, cacheExchange, createClient, fetchExchange, ssrExchange } from "@urql/next";
 import Head from "next/head";
-import { useMemo } from "react";
+import { Provider } from "urql";
 import NavBar from "./components/NavBar";
 import { inter } from "./fonts";
 import "./globals.css";
+import { createUrqlClient } from "./lib/urqlClient";
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    const [client, ssr] = useMemo(() => {
-        const ssr = ssrExchange({
-            isClient: typeof window !== "undefined"
-        });
-        // const client = createUrqlClient();
-        const client = createClient({
-            url: "http://localhost:4000/graphql",
-            exchanges: [cacheExchange, ssr, fetchExchange],
-            suspense: true,
-        });
-        return [client, ssr];
-    }, []);
     return (
-        <UrqlProvider client={client} ssr={ssr}>
+        <Provider value={createUrqlClient()}>
             <html lang="en">
                 <Head>
                     <title>Reddit Clone</title>
@@ -39,7 +27,7 @@ export default function RootLayout({
                     </ChakraProvider>
                 </body>
             </html>
-        </UrqlProvider>
+        </Provider>
 
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 "use client";
 import { Suspense } from "react";
 import Post from "./components/Post";
-import { Post as PostType, PostsDocument } from "./lib/graphql/generated/graphql";
-import { useQuery } from "@urql/next";
+import { Post as PostType, PostsDocument, usePostsQuery } from "./lib/graphql/generated/graphql";
 
 const Page = () => {
-    const [{ data }] = useQuery({ query: PostsDocument });
+    const [{ data }] = usePostsQuery();
     return (
         <Suspense>
             <h1>Posts</h1>
             {
                 data
-                    ? (data.posts.map((p: PostType) => (
-                        <Post key={p.id} title={p.title} content={p.content} />
+                    ? (data.posts.map(({ id, title, content }) => (
+                        <Post key={id} title={title} content={content} />
                     )))
                     : null
             }
